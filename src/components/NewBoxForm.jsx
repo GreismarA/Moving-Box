0898ac09cb1d45aa09279935a4e47dc4175b9858
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-03-11 16:18:50 -0400

    form update and firebase as dependency

--- src/components/NewBoxForm.jsx
+++ src/components/NewBoxForm.jsx
@@ -11,16 +11,28 @@ const NewBoxForm = () => {
     ubicacion: '',
     peso: 0,
     propietario: '',
+    tipoDeContenido: 'resistente',
+    nota: '',
   }
 
   const [data, setData] = useState(initialState)
 
+
+
+
+
   const handleOnChange = e => {
-    setData({...data, nombre: e.target.value})
+    const { name, value } = e.target;
+    setData({...data, [name]: value})
   }
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
+    try {
+  
+    } catch (error) {
+      
+    }
   }
 
   return (
@@ -28,7 +40,7 @@ const NewBoxForm = () => {
     <form>
       {/* nombre de la caja */}
       <div >
-        <label htmlFor="nombre" >Nombre de la caja</label>
+        <label htmlFor="nombre" >Descripcion</label>
         <input onChange={handleOnChange} name="nombre" id="nombre" type="text"/>
         
       </div>
@@ -64,25 +76,42 @@ const NewBoxForm = () => {
       {/* ubicacion final */}
       <div>
         <label htmlFor="ubicacion">Ubicacion final</label>
-        <input onChange={handleOnChange} name="ubicacion" type="text" />
+        <input onChange={handleOnChange} name="ubicacion" type="text" id="ubicacion" />
         
       </div>
 
       {/* peso */}
       <div>
         <label htmlFor="peso">Peso (opcional)</label>
-        <input onChange={handleOnChange} name="peso" type="number" />
+        <input onChange={handleOnChange} name="peso" type="number" id="peso" />
         
       </div>
 
       {/* propietario */}
       <div>
-        <label htmlFor="propietario"></label>
-        <input onChange={handleOnChange} name="propietario" type="text" />
-        
+        <label htmlFor="propietario">Propietario</label>
+        <input onChange={handleOnChange} name="propietario" type="text" id="propietario" />
       </div>
 
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      {/* tipo de contenido */}
+      <div>
+        <label htmlFor="tipoDeContenido" >Tipo de contenido</label>
+        <select onChange={handleOnChange} name="tipoDeContenido" id="tipoDeContenido">
+          <option value="resistente">Resistente</option>
+          <option value="fragil">Fragil</option>
+          <option value="noTanFragil">No tan fragil</option>
+        </select>
+
+        {/* <input onChange={handleOnChange} name="tipoDeContenido" type="text" id="tipoDeContenido" /> */}
+      </div>
+
+      {/* nota */}
+      <div>
+        <label htmlFor="nota">nota</label>
+        <input onChange={handleOnChange} name="nota" type="text" id="nota" />
+      </div>
+
+      <button type="submit" onClick={handleSubmit} >Submit</button>
     </form>
   )
 }
