b5b65aca9cfe6d6d938d8d6792caf4dfe55151cf
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-03-12 09:21:54 -0400

    scroll bug fixed

--- src/components/pages/home/buttonAction/CallToAction.jsx
+++ src/components/pages/home/buttonAction/CallToAction.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router";
 import img from "../../../../img/cajas2.png";
 import './CallToAction.css'
 
 function CallToAction() {
+
+    const navigate = useNavigate()
+    
+    const handleClickEmpzarBtn = () => {
+        navigate('/box')
+    }
+
     return (
         <>
             <section id="crear" className=" lg:grid grid-cols-[32%_68%]">
@@ -14,7 +22,12 @@ function CallToAction() {
                         <p className="text-black p-2 mb-10">Moving Box es una aplicación que te permitirá llevar el registro y control de las cajas de tu mudanza, para que no pierdas detalle del contenido de las mismas. Podrás crear cuantas cajas desees, y mediante un codigo QR tendrás toda la información de tu mudanza. </p>
                     </div>
                     <div className="flex justify-center items-end mb-[50px] border-red-500">
-                        <button className="py-3 px-8 rounded font-medium bg-[#e2e756] hover:bg-[#f5f89f]">Crear caja</button>
+                        <button
+                            className="py-3 px-8 rounded font-medium bg-[#e2e756] hover:bg-[#f5f89f]"
+                            onClick={handleClickEmpzarBtn}
+                        >
+                            Crear caja
+                        </button>
                     </div>
                 </div>
             </section>
