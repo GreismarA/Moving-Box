c5ca6c7cc918c850a4d0741f49f044b8ded3473b
VlaCas <34453102+VlaCas@users.noreply.github.com>
2023-03-12 00:25:54 -0400

    agregada la estructura de la page box

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Box from './pages/Box';
 import Home from './components/pages/home/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/box' element={<Box/>}></Route>
     </Routes>
   </BrowserRouter>  
 )
