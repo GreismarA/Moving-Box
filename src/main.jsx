7fbb83dc2a618de9991156ca804cac0f3287c080
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-03-13 23:27:25 -0400

    element component changed

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import Box from './pages/Box';
 import Home from './components/pages/home/Home'
 import NewBoxForm from "./components/NewBoxForm/NewBoxForm"
 import './index.css'
-import BoxDetails from './components/BoxDetails';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<BoxDetails/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
       <Route path='/box' element={<Box/>}></Route>
       <Route path='/form' element={<NewBoxForm/>}></Route>
     </Routes>
